96c1f55ecd633728168acde0a59157f08f046335
Ben <bebuttig@microsoft.com>
2020-04-23 13:57:44 +0100

    Created project

---
+++ LetMeIn/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using LetMeIn.Models;
+
+namespace LetMeIn.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
