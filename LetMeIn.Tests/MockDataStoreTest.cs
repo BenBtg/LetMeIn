6e626b08d53dd6b778f5a49ac7e5d4d44923e407
Ben <bebuttig@microsoft.com>
2020-04-24 14:43:37 +0100

    Added Unit Test project

---
+++ LetMeIn.Tests/MockDataStoreTest.cs
@@ -0,0 +1,35 @@
+﻿using LetMeIn.Models;
+using LetMeIn.Services;
+using NuGet.Frameworks;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetMeIn.Tests
+{
+    public class MockDataStoreTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public async Task AddItemTestAsync()
+        {
+            // Arrange
+            MockDataStore dataStore = new MockDataStore();
+            var item = new Item {Id="1", Text = "Test" };
+
+            // Act
+            var added = await dataStore.AddItemAsync(item);
+
+            // Asset
+            Assert.IsTrue(added);
+            var item2 = await dataStore.GetItemAsync(item.Id);
+            Assert.AreEqual(item, item2);
+        }
+    }
+}
